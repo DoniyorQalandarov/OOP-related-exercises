d570df7ed6b672fc105ad039e4ab001166de597d
doniyor9936 <103168279+DoniyorQalandarov@users.noreply.github.com>
2022-04-11 14:41:15 +0500

    Add files via upload

---
+++ MyMethod.java
@@ -0,0 +1,14 @@
+package leetCodeExamples.oopLearn;
+
+public class MyMethod {
+    public static void main(String[] args) {
+        Test testObj = new Test();
+        testObj.myMethod();
+    }
+}
+
+class Test {
+    public void myMethod() {
+        System.out.println("This is simple method");
+    }
+}
